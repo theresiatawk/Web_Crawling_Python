7e7444eb2f7d90813771199c1aad85fef88cf0ae
Theresia Tawk <theresiatawk@hotmail.com>
2023-03-22 18:00:48 +0200

    Implementing testing dirextories function

---
+++ subdomains_output.bat
@@ -0,0 +1,2 @@
+mail.google.com
+www.google.com
--- test_script.py
+++ test_script.py
@@ -13,14 +13,14 @@ subdomains_output = []
 directories_output = []
 
 def testingSubdomains(target_url):
+    open("./subdomains_output.bat", "w").close()
     try: 
         requests.get("http://"+target_url)
         input_file =  open("./input_files/subdomains_dictionary.bat", "r")
         subdomains_output_file = open("./subdomains_output.bat", "a")
         for line in input_file: 
             subdomain = line.strip()
-            url = ""
-            if subdomain[len(url)-1] == "." :
+            if subdomain[len(subdomain)-1] == "." :
                 url = subdomain +""+ target_url
             else:
                 url = subdomain +"."+ target_url
@@ -40,23 +40,47 @@ def testingSubdomains(target_url):
         print("No such domain")
     return subdomains_output
 
-def fetshingHTMLFiles(target_url):
+def testingDirectories(target_url):
+    open("./directories_output.bat", "w").close()
+    try:
+        requests.get("http://"+target_url)
+        input_file =  open("./input_files/dirs_dictionary.bat", "r")
+        directories_output_file = open("./directories_output.bat", "a")
+        for line in input_file: 
+            directory = line.strip()
+            url = target_url +"/"+ directory
+            print(url)
+            try:
+                response = requests.get("http://" + url)
+                if response.status_code == 200:
+                    directories_output.append(url)
+                    directories_output_file.write(url +"\n")
+                    print(f"This directory {url} exist !!!!!!!!!!!!!!!")
+                    print(directories_output)
+            except requests.exceptions.ConnectionError:
+                pass
+        input_file.close()
+        directories_output_file.close()
+    except requests.exceptions.ConnectionError:
+        print("No such domain")
+    return directories_output
+    
+
+def fetshingFiles(target_url):
+    open("./files_output.bat", "w").close()
     files_output_file = open("./files_output.bat", "a")
     try:
         response = requests.get("http://"+target_url)
         links =  re.findall('href="(.*?)"', response.content.decode('utf-8'))
-        for link in links: 
-            print(link)
-            link = urljoin(target_url)
+        for link in links:
             files_output_file.write(link +"\n")
         files_output_file.close()
         return links
     except requests.exceptions.ConnectionError:
         return "No such domain"
-
-    
+  
 url1 = "google.com"
 url2 = "testphp.vulnweb.com"
-# print(testingSubdomains(url2))
-print(fetshingHTMLFiles(url2))
-# print(requests.get("https://" + url))
+# print(testingSubdomains(url1))
+# print(fetshingFiles(url2))
+print(testingDirectories(url1))
